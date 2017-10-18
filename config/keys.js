a3b050de6212865383395bea028cea47ef06b6ba
dustinddoan <dustin.ddoan@gmail.com>
2017-10-18 15:53:22 -0700

    add prod env

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // COMMIT THIS 
 if (process.env.NODE_ENV === 'production') {
   // we are in production 
-  module.export = require('./prod')
+  module.exports = require('./prod')
 } else {
   // we are in dev 
   module.exports = require('./dev');
