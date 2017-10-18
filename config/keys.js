4051488080e0c4cc5c276eea9abe5078aa71bc02
dustinddoan <dustin.ddoan@gmail.com>
2017-10-18 15:45:59 -0700

    fix prod env

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // COMMIT THIS 
 if (process.env.NODE_ENV === 'production') {
   // we are in production 
-  modules.export = require('./prod')
+  module.export = require('./prod')
 } else {
   // we are in dev 
   module.exports = require('./dev');
