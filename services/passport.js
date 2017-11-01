2173a92fa1144ff2fe5d7cd3e867b2ff3c388b91
dustinddoan <dustin.ddoan@gmail.com>
2017-11-01 16:48:41 -0700

    add engine stricts

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const GoogleStrategry = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
